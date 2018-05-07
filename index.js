5560a719126d1c5c2b76d463c2adfd5f819de651
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-07 10:55:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(musicians,instruments) {
 }
 
 function johnLennonFacts(facts) {
-  while (x<facts.length) {
+  while (var x<facts.length) {
     console.log(facts[x]+"!!!")
     x+=1
   }
